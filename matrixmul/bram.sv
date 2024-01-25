87ded91748166115160cc43b9f89b379f0190949
MorZao <156161660+MorZao@users.noreply.github.com>
2024-01-25 11:43:53 -0600

    Update bram.sv

--- matrixmul/bram.sv
+++ matrixmul/bram.sv
@@ -25,3 +25,33 @@ module bram
   end
 
 endmodule
+
+
+module bram 
+#(parameter BRAM_ADDR_WIDTH = 6,
+  parameter BRAM_DATA_WIDTH = 32) 
+(
+  input  logic clock,
+  input  logic [BRAM_ADDR_WIDTH-3:0] rd_addr,
+  input  logic [BRAM_ADDR_WIDTH-3:0] wr_addr,
+  input  logic wr_en,
+  input  logic [BRAM_DATA_WIDTH-3:0] din, 
+  output logic [BRAM_DATA_WIDTH-3:0] dout
+);
+
+  // 内存定义
+  logic [2**BRAM_ADDR_WIDTH-1:0][BRAM_DATA_WIDTH-1:0] mem;
+
+  // 组合逻辑读操作
+  always_comb begin
+    dout = mem[rd_addr]; // 使用rd_addr直接读取，而不是read_addr
+  end
+  
+  // 时序逻辑写操作
+  always_ff @(posedge clock) begin
+    if (wr_en) begin
+      mem[wr_addr] <= din; // 时序写入
+    end
+  end
+
+endmodule
