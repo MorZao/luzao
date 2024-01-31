13d7e2a4875ebc360c434c6d0e20d163057386cc
MorZao <156161660+MorZao@users.noreply.github.com>
2024-01-31 01:39:45 -0600

    Update subtract.sv

--- subtract/subtract.sv
+++ subtract/subtract.sv
@@ -7,8 +7,8 @@ module subtract #(
     output logic        in_rd_en_img,
     input  logic        in_empty_base,
     input  logic        in_empty_img,
-    input  logic [7:0]  in_dout_base,
-    input  logic [7:0]  in_dout_img,
+    input  logic [23:0] in_dout_base,
+    input  logic [23:0] in_dout_img,
     output logic        out_wr_en,
     input  logic        out_full,
     output logic [7:0]  out_din
@@ -19,7 +19,7 @@ state_types state, state_c;
 
 logic [7:0] sub, sub_c;
 logic [8:0] diff;
-logic [7:0] diff_abs
+logic [8:0] diff_abs;
 
 always_ff @(posedge clock or posedge reset) begin
     if (reset == 1'b1) begin
@@ -32,8 +32,8 @@ always_ff @(posedge clock or posedge reset) begin
 end
 
 always_comb begin
-    in_rd_en_base  = 1'b0;
-    in_rd_en_img  = 1'b0;
+    //in_rd_en_base  = 1'b0;
+    //in_rd_en_img  = 1'b0;
     out_wr_en = 1'b0;
     out_din   = 8'b0;
     state_c   = state;
@@ -41,15 +41,15 @@ always_comb begin
 
     case (state)
         s0: begin
-            if (in_empty_base && in_empty_img == 1'b0) begin
-                diff = {1'b0, in_dout_img} - {1'b0, in_dout_base};
-                if (diff[8]) begin
-                    diff_abs = -diff[7:0]; 
-                end else begin
-                    diff_abs = diff[7:0];
-                end
-                sub_c = diff_abs > THRESHOLD ? 0xFF : 0x00;
-                in_rd_en = 1'b1;
+            if (in_empty_base == 1'b0 && in_empty_img == 1'b0) begin //&& in_empty_img
+                diff = {1'b0, in_dout_img[7:0]} - {1'b0, in_dout_base[7:0]};
+                case (diff[8]) 
+                    1'b0:diff_abs = diff;
+                    1'b1:diff_abs = ~diff + 1'b1;
+                endcase
+                sub_c = diff_abs > THRESHOLD ? 8'hFF : 8'b00;
+                in_rd_en_base = 1'b1;
+                in_rd_en_img = 1'b1;
                 state_c = s1;
             end
         end
